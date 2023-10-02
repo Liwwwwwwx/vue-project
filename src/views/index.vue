b5896bd7088cc7a590d765d2c84a95577bb31b14
“Liwwwwwwx” <hbsd_lwx@163.com>
2023-10-02 19:15:05 +0800

    🌈 style(主页): 将内容显示居中显示

--- src/views/index.vue
+++ src/views/index.vue
@@ -2,24 +2,24 @@
  * @Author: “Liwwwwwwx” 1076843408@qq.com
  * @Date: 2023-09-15 10:41:18
  * @LastEditors: “Liwwwwwwx” hbsd_lwx@163.com
- * @LastEditTime: 2023-09-27 17:27:13
+ * @LastEditTime: 2023-10-02 19:10:18
  * @FilePath: /vue-project/src/views/index.vue
  * @Description: 布局
 -->
 <template>
-  <div class="login">
-    <SimSwitchesConfig></SimSwitchesConfig>
-    <SimReadExcel></SimReadExcel>
+  <div class="base_container">
+    <div class="container">
+      <SimReadExcel></SimReadExcel>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import SimSwitchesConfig from "@/components/SimConfig/SimSwitchesConfig.vue";
-import SimReadExcel from '@/components/SimConfig/SimReadExcel.vue'
+import SimReadExcel from "@/components/SimConfig/SimReadExcel.vue";
 </script>
 
 <style scoped lang="scss">
-.login {
+.base_container {
   background: url(../assets/img/login_bg.png);
   background-size: cover;
   height: 100%;
@@ -29,8 +29,11 @@ import SimReadExcel from '@/components/SimConfig/SimReadExcel.vue'
   left: 0;
   right: 0;
   bottom: 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
+  .container {
+    width: 80%;
+    margin: 5rem auto;
+    display: flex;
+    flex-direction: column;
+  }
 }
 </style>
